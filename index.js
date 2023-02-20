bd9c58a53b34383d5c77096982a66cd47dfea703
unknown <baudinopierreolivier@gmail.com>
2023-02-20 16:47:51 +0100

    checkbox verify

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // console.log("test");
+// console.log(document.forms[0]);
+
+var inputsCheck;
+const testCheck = document.getElementById("testCheck");
+var inputs = document.querySelectorAll("input[type=checkbox]");
+
+function testChecked() {
+  inputsCheck = document.querySelectorAll("input:checked");
+  console.log(inputsCheck.length);
+  if (inputsCheck.length > 0) {
+    testCheck.classList.remove("not-valid");
+    inputs.forEach((e) => {
+      e.classList.remove("not-valid");
+    });
+  }
+  if (inputsCheck.length <= 0) {
+    testCheck.classList.add("not-valid");
+  }
+  if (inputsCheck.length == 5) {
+    inputs.forEach((e) => {
+      e.classList.add("not-valid");
+    });
+  }
+}
+
+inputs.forEach((e) => {
+  e.addEventListener("click", testChecked);
+});
